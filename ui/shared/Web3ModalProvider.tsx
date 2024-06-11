2a4503fe4dd66cc237a245ae3316787410fc49b7
gitcodeporter <gitcodeporter@gmail.com>
2024-06-11 19:33:14 +0800

    mod

--- ui/shared/Web3ModalProvider.tsx
+++ ui/shared/Web3ModalProvider.tsx
@@ -46,7 +46,6 @@ const getConfig = () => {
       },
       featuredWalletIds: [
         '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
-        '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
         'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
       ],
     });
