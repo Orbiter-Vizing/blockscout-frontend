b2d3f00f0e2879507f7d098bd4aee64e0ca4ed5d
coke <coke10086@gmail.com>
2024-06-11 10:11:34 +0800

    add wallet

--- ui/shared/Web3ModalProvider.tsx
+++ ui/shared/Web3ModalProvider.tsx
@@ -1,6 +1,6 @@
 import { useColorMode } from '@chakra-ui/react';
 import { jsonRpcProvider } from '@wagmi/core/providers/jsonRpc';
-import { createWeb3Modal, useWeb3ModalTheme, defaultWagmiConfig } from '@web3modal/wagmi/react';
+import { createWeb3Modal, defaultWagmiConfig, useWeb3ModalTheme } from '@web3modal/wagmi/react';
 import React from 'react';
 import { configureChains, WagmiConfig } from 'wagmi';
 
@@ -46,13 +46,14 @@ const getConfig = () => {
       },
       featuredWalletIds: [
         '38f5d18bd8522c244bdd70cb4a68e0e718865155811c043f052fb9f1c51de662',
+        '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0',
         'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
       ],
     });
 
     return { wagmiConfig };
   } catch (error) {
-    return { };
+    return {};
   }
 };
 
